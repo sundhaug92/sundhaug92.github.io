a6a5ea1f98036b4269297dac21825d66dc724022
Martin Sundhaug <martinsundhaug@gmail.com>
2024-02-20 17:29:26 +0100

    Add debug-script

---
+++ debug.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Prerequisites
+# 1. Install Ruby
+# 2. Install Bundler
+# 3. Install Jekyll
+
+# See https://jekyllrb.com/docs/installation/
+# See https://jekyllrb.com/docs/
+
+bundle exec jekyll serve --livereload
